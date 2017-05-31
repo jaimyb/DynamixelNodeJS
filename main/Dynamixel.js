91540a7484cb7b6f9e36e2e09ff9e608aff70889
Jaimy Beima <jaimybeima@live.nl>
2017-05-31 14:07:12 +0200

    Data write completed
    Sends data correctly but doesn't get any back.

---
+++ main/Dynamixel.js
@@ -0,0 +1,51 @@
+var SerialPort = require('serialport');
+var Gpio = require('pigpio').Gpio;
+var motor = new Gpio(18, {mode:Gpio.OUPUT});
+var buffer = new ArrayBuffer(9);
+var uint8 = new Uint8Array(buffer);
+
+port = new SerialPort('/dev/ttyAMA0', {
+	baudrate: 1000000,
+	parser: SerialPort.parsers.readline("\n")
+});
+
+port.on("open", function () {
+ 	console.log('open');
+  	port.on('data', function(data) {
+    	console.log("Data received!");
+  	});
+});
+
+function getData(ID){
+	
+	var start = 0xFF;
+	var id = ID;
+	var length =  0x04;
+	var ins = 0x02;
+	var param1 = 0x2B;
+	var param2 = 0x01;
+
+
+	var checksum = ~(id + length + ins + param1 + param2) & 0xFF;
+	
+	motor.digitalWrite(1);
+
+	uint8.set([start, start, id, length, ins, param1, param2, checksum]);
+	port.write(uint8);
+
+	setTimeout(function(){
+		motor.digitalWrite(0);
+	},0);
+
+	
+	console.log(checksum);
+	console.log(uint8);
+
+
+}
+
+setTimeout(function(){
+	getData(1);
+},100);
+
+
